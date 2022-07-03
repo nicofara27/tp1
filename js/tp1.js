a482f5a655a96407201d6b62c2d704344c64baf0
NicoFara <nicolas.fara.romero@gmail.com>
2022-07-03 11:13:26 -0300

    Finalizacion del tercer punto

--- js/tp1.js
+++ js/tp1.js
@@ -1,7 +1,11 @@
 /* 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”. */
 
-alert("un mensaje");
+// alert("un mensaje");
 
 /* 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write). */
 
-document.write("Hello World");
+// document.write("Hello World");
+
+/* 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5. */
+
+document.write(3+5);
