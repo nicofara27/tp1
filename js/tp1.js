09adc14aad30220aa255207a4db15642b73c2f7e
NicoFara <nicolas.fara.romero@gmail.com>
2022-07-03 11:17:10 -0300

    Finalizacion del cuarto punto

--- js/tp1.js
+++ js/tp1.js
@@ -8,4 +8,10 @@
 
 /* 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5. */
 
-document.write(3+5);
+// document.write(3+5);
+
+/* 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario» */
+
+let nombre = prompt("Cual es tu nombre?");
+document.write("Hola " + nombre);
+
